afdfdc0940efd73114a1b978e8f038b96840890b
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-03-13 09:17:43 +0900

    :recycle: BrandモデルのRSpecを他にRSpecと同じ形式に合わせる

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -4,15 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
   describe 'validations' do
-    let(:brand) { build(:brand, name: brand_name) }
     let(:errors) do
       brand.valid?
       brand.errors
     end
 
     context 'when name' do
-      context 'when nil is not present' do
-        let(:brand_name) { nil }
+      context 'when nil' do
+        let(:brand) { build(:brand, name: nil) }
         let(:nil_error_message) { ['ブランド名は空で登録できません'] }
 
         it { expect(brand).to be_invalid }
