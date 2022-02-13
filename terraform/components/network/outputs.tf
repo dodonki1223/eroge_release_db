6c62923d15a8bcffcc58c300ccf4dd3e18f02190
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-02-13 19:21:00 +0900

    :sparkles: RDS 用のセキュリティグループを追加

--- terraform/components/network/outputs.tf
+++ terraform/components/network/outputs.tf
@@ -21,5 +21,6 @@ output "private_subnet_ids" {
 output "security_group_ids" {
   value = {
     bastion = aws_security_group.bastion.id
+    rds     = aws_security_group.eroge_release_rds.id
   }
 }
--- terraform/components/network/security_group.tf
+++ terraform/components/network/security_group.tf
@@ -5,10 +5,10 @@ resource "aws_security_group" "bastion" {
 
   egress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = "0"
-    protocol    = "-1"
+    from_port   = 0
+    protocol    = -1
     self        = "false"
-    to_port     = "0"
+    to_port     = 0
   }
 
   ingress {
@@ -22,6 +22,43 @@ resource "aws_security_group" "bastion" {
 
   tags = {
     Name        = format("%s-bastion", var.application)
-    Description = format("Security group for creating %s resources", var.application)
+    Description = format("Bastion server Security group for creating %s resources", var.application)
+  }
+}
+
+resource "aws_security_group" "eroge_release_rds" {
+  name        = format("%s-rds", var.application)
+  description = format("Allows access to the db server for %s", var.application)
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = -1
+    self        = false
+    to_port     = 0
+  }
+
+  ingress {
+    description     = format("Allow DB(PostgreSQL) access only from a bastion server for %s", var.application)
+    from_port       = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+    self            = false
+    to_port         = 5432
+  }
+
+  ingress {
+    description     = format("Allow SSH access only from a bastion server for %s", var.application)
+    from_port       = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+    self            = false
+    to_port         = 22
+  }
+
+  tags = {
+    Name        = format("%s-rds", var.application)
+    Description = format("RDS Security group for creating %s resources", var.application)
   }
 }
