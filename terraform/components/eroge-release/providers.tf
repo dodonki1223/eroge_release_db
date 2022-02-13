2b7f4c998158f4721c251786c08ec9f364c0974f
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-02-13 15:59:40 +0900

    :sparkles: RDS の Terraform 化の準備完了

---
+++ terraform/components/eroge-release/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "1.1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.65.0"
+    }
+  }
+
+  backend "s3" {
+    region  = "ap-northeast-1"
+    encrypt = true
+    bucket  = "eroge-release-db"
+    key     = "eroge-release/terraform.tfstate"
+    profile = "terraform"
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+  default_tags {
+    tags = {
+      Component   = var.component
+      Application = var.application
+    }
+  }
+}
