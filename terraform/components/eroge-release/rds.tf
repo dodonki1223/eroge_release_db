ed3442ff0e8eea386bc8d24ee21ac0551be54c25
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-02-13 19:59:19 +0900

    :sparkles: DB  オプショングループを追加

--- terraform/components/eroge-release/rds.tf
+++ terraform/components/eroge-release/rds.tf
@@ -1,3 +1,15 @@
+resource "aws_db_option_group" "eroge_release" {
+  name                     = format("%s-postgres-11", var.application)
+  option_group_description = format("%s option group for postgres 11", var.application)
+  engine_name              = "postgres"
+  major_engine_version     = "11"
+
+  tags = {
+    Name        = format("%s-postgres-11", var.application)
+    Description = format("DB option group for creating %s resources", var.application)
+  }
+}
+
 resource "aws_db_subnet_group" "private" {
   name        = format("%s-db-private", var.application)
   description = format("%s-db-private", var.application)
